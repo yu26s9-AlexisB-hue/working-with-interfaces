497dae8027a2071d17e28015102fd9f782fbf63b
Alexis Barrett <happynessb09@gmail.com>
2026-05-19 15:33:12 -0400

    Creating the persons class

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(){
+        List<Person> myFamily = new ArrayList<>();
+        myFamily.add( new Person("Dana", "Wyatt", 63) );
+        myFamily.add( new Person("Zachary", "Westly", 31) );
+        myFamily.add( new Person("Elisha", "Aslan", 14) );
+        myFamily.add( new Person("Ian", "Auston", 16) );
+        myFamily.add( new Person("Zephaniah", "Hughes", 9) );
+        myFamily.add( new Person("Ezra", "Aiden", 17) );
+
+        Collections.sort(myFamily);
+    }
+}
---
+++ src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class Person implements Comparable<Person>{
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Person otherPerson){
+        return this.firstName.compareTo(otherPerson.firstName);
+    }
+
+    @Override
+    public String toString(){
+        return firstName + " " + lastName + "Age: " + age;
+    }
+}
